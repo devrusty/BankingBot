368a72354e00c2b98584b673b0f384bfd8a2cc06
devrusty <rguillory246@gmail.com>
2022-07-30 14:22:03 -0500

    made private along with command handler and balanc

---
+++ commands/balance.ts
@@ -0,0 +1,32 @@
+import Command from "../interfaces/commandInterface";
+import { Client, Message } from "discord.js";
+import * as DatabaseMethods from "..//databaseMethods"
+import { EmbedBuilder } from "discord.js";
+
+const Cmd: Command = {
+    Name: "balance",
+    Description: "Shows your balance if you have a BankingBot account initialised.",
+    Usage: `${process.env.PREFIS}balance`,
+    Invoke: async (client: Client, message: Message) => {
+        const recordExists: boolean = await DatabaseMethods.UserRecordExists(message.author)
+        if (!recordExists) {
+            message.channel.send("You do not have a BankingBot account! Please use `b!create` to initialise one.")
+            return
+        }
+
+        const balance: number = await DatabaseMethods.GetUserBalance(message.author)
+        const embed: EmbedBuilder = new EmbedBuilder()
+
+        embed.setTitle(`${message.author.username}'s Balance`)
+        embed.setThumbnail(message.author.displayAvatarURL())
+        embed.setFields(
+            { name: "Balance", value: String(balance) }
+        )
+
+        message.channel.send({
+            embeds: [embed]
+        })
+    }
+}
+
+export default Cmd
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { Client, Message, EmbedBuilder } from "discord.js"
-import * as DatabaseMethods from "./databaseMethods"
+import { Client, Message } from "discord.js"
 import * as dotenv from "dotenv"
+import * as fs from "fs"
 
 const Bot: Client = new Client({
     intents: [
@@ -21,32 +21,16 @@ Bot.on("messageCreate", async (message: Message) => {
     if (!message.member) return
 
     const args = message.content.trim().split(/ +/g)
-    const command = args[0].slice("b!".length).toLowerCase()
+    const command: string = args[0].slice("b!".length).toLowerCase()
 
-    if (command == "create") {
-
+    const commandFilePath: string = `./commands/${command}.ts`
+    if (!fs.existsSync(commandFilePath)) {
+        message.channel.send(`Invalid command \`${command}\`. Please use \`b!help\` to view a list of commands.`)
+        console.log(commandFilePath)
+        return
     }
 
-    if (command == "balance") {
-        const recordExists: boolean = await DatabaseMethods.UserRecordExists(message.author)
-        if (!recordExists) {
-            message.channel.send("You do not have a BankingBot account! Please use `b!create` to initialise one.")
-            return
-        }
-
-        const balance: number = await DatabaseMethods.GetUserBalance(message.author)
-        const embed: EmbedBuilder = new EmbedBuilder()
-
-        embed.setTitle(`${message.member.user.username}'s Balance`)
-        embed.setThumbnail(message.member.user.displayAvatarURL())
-        embed.setFields(
-            { name: "Balance", value: String(balance) }
-        )
-
-        message.channel.send({
-            embeds: [embed]
-        })
-    }
+    await require(commandFilePath).default.Invoke(Bot, message)
 })
 
-Bot.login(process.env.BOT_TOKEN)
+Bot.login("MTAwMjY5ODg5MTUzNzQyNDM4NA.GQoqi1.QJkAaT1b75YIFd0hCqH54dTYPuV3nzN9luyNt4")
