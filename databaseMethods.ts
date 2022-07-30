8482bd295ed82cfb7e6c80eb7a843833c9caf73d
devrusty <rguillory246@gmail.com>
2022-07-29 19:44:20 -0500

    removed duplicate prismaclients

--- databaseMethods.ts
+++ databaseMethods.ts
@@ -2,30 +2,25 @@ import { prisma, PrismaClient } from "@prisma/client"
 import { User } from "@prisma/client"
 import { GuildMember } from "discord.js"
 
-function CreatePrismaClient(): PrismaClient {
-    return new PrismaClient()
-}
+const PClient: PrismaClient = new PrismaClient()
 
 export async function UserRecordExists(user: GuildMember): Promise<boolean> {
-    const client: PrismaClient = CreatePrismaClient()
-    const userResult = await client.user.count({
+    const userResult = await PClient.user.count({
         where: {
             id: Number(user.id)
         }
     })
 
-    await client.$disconnect()
     return userResult > 0
 }
 
 export async function CreateUserRecord(user: GuildMember): Promise<boolean> {
     const id: number = Number(user.id)
-    const client: PrismaClient = CreatePrismaClient()
 
     const userExists: boolean = await UserRecordExists(user)
     if (userExists) return false
 
-    await client.user.create({
+    await PClient.user.create({
         data: {
             id: id
         }
@@ -33,23 +28,20 @@ export async function CreateUserRecord(user: GuildMember): Promise<boolean> {
         console.trace(err)
     })
 
-    await client.$disconnect()
     return true
 }
 
 export async function GetUserBalance(user: GuildMember) {
     const id: number = Number(user.id)
-    const client: PrismaClient = CreatePrismaClient()
 
     const userExists: boolean = await UserRecordExists(user)
     if (!userExists) return 0
 
-    const userRecord = await client.user.findFirst({
+    const userRecord = await PClient.user.findFirst({
         where: {
             id: id
         }
     })
-    await client.$disconnect()
     if (!userRecord) return 0
 
     return userRecord.coins
