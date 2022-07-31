2e6738deae9051455ec437443c0f42520f1b1a35
devrusty <rguillory246@gmail.com>
2022-07-31 02:23:47 -0500

    fix

--- commands/help.ts
+++ commands/help.ts
@@ -3,9 +3,10 @@ import { Client, Message, EmbedBuilder } from "discord.js"
 import * as fs from "fs"
 
 const GetCommandFields = () => {
-    const commands = fs.readdirSync("./commands/").filter(file => file !== "help.ts")
+    const commands = fs.readdirSync("./commands/").filter(command => command !== "help.ts")
     return commands.map(command => {
         const data: Command = require(`./${command}`).default
+
         return {
             name: data.Name,
             value: data.Usage,
