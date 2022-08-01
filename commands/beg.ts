5adb66308114069abd6c473eaea0a846efec5c8d
devrusty <rguillory246@gmail.com>
2022-08-01 00:25:56 -0500

    added beg command

---
+++ commands/beg.ts
@@ -0,0 +1,39 @@
+import Command from "../interfaces/commandInterface";
+import { Client, Message } from "discord.js"
+import * as DatabaseMethods from "../databaseMethods"
+
+const Cooldown = 120000
+const MaxAmount = 1000
+
+const RecentlyBegged = new Set<string>()
+const Cmd: Command = {
+    Name: "beg",
+    Description: "Gives you a random amount of cash from 1 - 1000. Can only be used once every 2 minutes.",
+    Usage: "b!beg",
+    Listed: true,
+    Invoke: async (client: Client, message: Message) => {
+        const author = message.author
+        const userRecordExists = await DatabaseMethods.UserRecordExists(author)
+        if (!userRecordExists) {
+            message.channel.send("You must have a BankingBot account initialised to use `b!beg`. Please use `b!create`")
+            return
+        }
+
+        if (RecentlyBegged.has(author.id)) {
+            message.channel.send("Please wait 2 minutes before begging again.")
+            return
+        }
+
+        const randomAmount = Math.floor(Math.random() * MaxAmount)
+        DatabaseMethods.AddToBalance(author, randomAmount).then(() => {
+            message.channel.send(`You got ${randomAmount} from begging.`)
+            RecentlyBegged.add(author.id)
+
+            setTimeout(() => {
+                RecentlyBegged.delete(author.id)
+            }, Cooldown)
+        })
+    }
+}
+
+export default Cmd
