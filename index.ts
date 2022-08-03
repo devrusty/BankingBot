74b31a6ad776fb59bea19ce315a689e7537df05f
devrusty <rguillory246@gmail.com>
2022-08-02 21:30:24 -0500

    created configuration json

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Client, Message, ActivityType } from "discord.js"
 import * as fs from "fs"
 import * as ItemShopMethods from "./methods/ItemShop"
+import * as Config from "./config.json"
 
 const Bot: Client = new Client({
     intents: [
@@ -33,19 +34,19 @@ Bot.on("ready", async () => {
 
 Bot.on("messageCreate", async (message: Message) => {
     if (message.author.bot) return
-    if (!message.content.startsWith("b!")) return
+    if (!message.content.startsWith(Config.prefix)) return
     if (!message.member) return
 
     const args = message.content.trim().split(/ +/g)
-    const command: string = args[0].slice("b!".length).toLowerCase()
+    const command: string = args[0].slice(Config.prefix.length).toLowerCase()
 
     const commandFilePath: string = `./commands/${command}.ts`
     if (!fs.existsSync(commandFilePath)) {
-        message.channel.send(`Invalid command \`${command}\`. Please use \`b!help\` to view a list of commands.`)
+        message.channel.send(`Invalid command \`${command}\`. Please use \`b!help commands\` to view a list of commands.`)
         return
     }
 
     await require(commandFilePath).default.Invoke(Bot, message, args)
 })
 
-Bot.login("MTAwMjY5ODg5MTUzNzQyNDM4NA.GQoqi1.QJkAaT1b75YIFd0hCqH54dTYPuV3nzN9luyNt4")
+Bot.login(Config.token)
