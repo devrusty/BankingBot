73a8213b949c63db1acf251b63615eaaaa76286d
devrusty <rguillory246@gmail.com>
2022-08-18 10:29:51 -0500

    updated itemshop algorithm

--- methods/ItemShop.ts
+++ methods/ItemShop.ts
@@ -1,20 +1,26 @@
 import * as DatabaseMethods from "../databaseMethods"
+import { Item } from "@prisma/client"
 
 export async function UpdateItemShop() {
     console.log("Updating item shop..")
     const itemShopItems = await DatabaseMethods.GetItems()
-
     itemShopItems.forEach(item => {
         return item.onSale = false
     })
 
-    for (let i = 0; i < itemShopItems.length; i++) {
+    const items: Item[] = new Array<Item>()
+
+    for (let i = 0; i < 9; i++) {
         const randomIndex = Math.floor(Math.random() * itemShopItems.length)
         const item = itemShopItems[randomIndex]
-        item.onSale = true
+
+        if (!items.includes(item)) {
+            item.onSale = true
+            items.push(item)
+        }
     }
 
-    await DatabaseMethods.UpdateItemShop(itemShopItems)
-
-    console.log("Updated item shop!")
+    await DatabaseMethods.UpdateItemShop(items).then(() => {
+        console.log("Updated item shop!")
+    })
 }
