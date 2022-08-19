932d4342edfa6b1258c67a290e57dc4a9af93a35
devrusty <rguillory246@gmail.com>
2022-08-18 20:12:15 -0500

    added increased donation limit field to help.premi

--- commands/help.ts
+++ commands/help.ts
@@ -66,6 +66,7 @@ const Embeds = {
             { name: "💎 Premium Role", value: "Gives you a premium role in the BankingBot support role", inline: true },
             { name: "🎲 x1.50 Gambling", value: "Multiplies gambling amount by x1.5", inline: true },
             { name: "🎒 Increased Inventory", value: "Inventory capacity is increased from 25 -> 100", inline: true },
+            { name: "💸 Increased Donation Limit", value: "Donation limit is increased from $50,000 -> $100,000", inline: true },
             { name: "Become a premium member", value: "[Link](https://ko-fi.com/s/d7a61f1814)", inline: false }
         )
 }
