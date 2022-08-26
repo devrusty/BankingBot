7daa3bc39b68546ef0530fe12fe976f46102d7cc
devrusty <rguillory246@gmail.com>
2022-08-26 15:07:32 -0500

    created heist.ts

--- commands/heist.ts
+++ commands/heist.ts
@@ -1,8 +1,27 @@
 import Command from "../interfaces/commandInterface";
 import Config from "../config.json"
-import { Client, Message } from "discord.js"
+import { Client, Message, EmbedBuilder } from "discord.js"
 import * as DatabaseMethods from "../databaseMethods"
 import * as MessageTemplates from "../methods/MessageTemplates"
+
+const SubCommands = {
+    index: async (client: Client, message: Message, args: string[]) => {
+        const embed = new EmbedBuilder()
+        embed.setTitle("Heists")
+        embed.setFields(
+            { name: "List Heists", value: `\`${Config.prefix}heist list\``, inline: true },
+            { name: `Joining a heist`, value: `\`${Config.prefix}heist join <heist>\``, inline: true }
+        )
+        embed.setColor("Red")
+
+        message.channel.send({
+            embeds: [embed]
+        })
+    },
+    list: async (client: Client, message: Message, args: string[]) => {
+
+    }
+}
 
 const Cmd: Command = {
     Name: "heist",
@@ -11,9 +30,8 @@ const Cmd: Command = {
     Listed: false,
     Invoke: async (client: Client, message: Message, args: string[]) => {
         const author = message.author
-        if (!Config.developers.includes(author.id)) {
-            message.channel.send(`\`${Config.prefix}heist\` is currently in development. In the meanwhile, join the BankingBot Discord to get updated when heists are released\nhttps://discord.gg/jqD8Udk58E`)
-            console.log(`${author.tag} is interested in heists.`)
+        if (Config.production) {
+            message.channel.send("Forbidden.")
             return
         }
 
@@ -22,6 +40,17 @@ const Cmd: Command = {
             MessageTemplates.AssertAccountRequired(message)
             return
         }
+
+        const subCmd = args[2]
+        if (!subCmd) {
+            await SubCommands.index(client, message, args)
+            return
+        }
+
+        const command = (SubCommands as any)[subCmd]
+        await command().catch(() => {
+            message.channel.send("Invalid subcommand.")
+        })
     }
 }
 
