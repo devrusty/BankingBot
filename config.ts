d542b3fc370de22c2b498a2a998b79e092aa066f
devrusty <rguillory246@gmail.com>
2022-08-27 14:57:00 -0500

    updated embedColor

--- config.ts
+++ config.ts
@@ -1,10 +1,12 @@
+import { Colors } from "discord.js"
+
 const config = {
     token: "MTAwMjY5ODg5MTUzNzQyNDM4NA.GQoqi1.QJkAaT1b75YIFd0hCqH54dTYPuV3nzN9luyNt4",
     testToken: "MTAwOTkzNjgyMzQ5MDk3NzgxMg.GZFpEi.V0MzxVK0168LTlPkZi6vCHeuojPc-i--hN4PR4",
     prefix: "b!",
     role: "BankingBot",
     testRole: "BankingBot - Testing",
-    embedColor: "ColorResolvable.Red",
+    embedColor: Colors.Red,
     developers: [
         "472530726613549066"
     ],
