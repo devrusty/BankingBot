2cbfe07c8aab55e050494298357c4e86f908ef05
devrusty <rguillory246@gmail.com>
2022-08-27 08:28:02 -0500

    created UpdateHeists

--- methods/Heists.ts
+++ methods/Heists.ts
@@ -1,5 +1,20 @@
+import { Heist } from "@prisma/client"
 import * as DatabaseMethods from "../databaseMethods"
+import HeistMeta from "../interfaces/HeistMeta"
 
 export async function UpdateHeists() {
+    const heists = await DatabaseMethods.GetHeists()
+    const updatedHeists = Array<Heist>()
 
+    heists.forEach((heist) => {
+        heist.avaliable = false
+    })
+
+    for (var i = 0; i < 3; i++) {
+        const heist = heists[i]
+        heist.avaliable = true
+        updatedHeists.push(heist)
+    }
+
+    return updatedHeists
 }
