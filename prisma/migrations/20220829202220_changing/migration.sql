7ce4b38ed474aeae67df2d310af8128fc8257e72
devrusty <rguillory246@gmail.com>
2022-08-29 15:22:29 -0500

    dropped users column

---
+++ prisma/migrations/20220829202220_changing/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `users` on the `Heist` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Heist" DROP COLUMN "users";
