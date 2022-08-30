adf88708c05993ce918ba8c99b99c9e8f83fa67b
devrusty <rguillory246@gmail.com>
2022-08-30 16:48:55 -0500

    created reset method

--- cache/GlobalHeistData.ts
+++ cache/GlobalHeistData.ts
@@ -1,19 +1,9 @@
 import HeistMeta from "../interfaces/HeistMeta";
-import * as DatabaseMethods from "../Database"
-import { User } from "discord.js";
 
 let GlobalHeistData: Array<HeistMeta> = new Array<HeistMeta>()
 
-async function Init() {
-    const Heists = await DatabaseMethods.GetAvaliableHeists()
-    GlobalHeistData = Heists.map((heist) => {
-        return {
-            Heist: heist,
-            Users: new Array<User>()
-        }
-    })
+export function reset() {
+    GlobalHeistData = new Array<HeistMeta>()
 }
-
-Init()
 
 export default GlobalHeistData
